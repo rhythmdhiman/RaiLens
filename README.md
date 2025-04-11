2d3fb29c90e545b13ee143de0759b626b38d75f6
Your Name <your-email@example.com>
2025-04-11 22:34:22 +0530

    first commit

--- README.md
+++
@@ -1,3 +0,0 @@
-# RailView
-
-[https://railviews.onrender.com/]
--- cloudinary/index.js
+++ cloudinary/index.js
@@ -10,7 +10,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary,
     params: {
-        folder: 'RailView',
+        folder: 'RaiLens',
         allowedFormats: ['jpeg', 'png', 'jpg']
     }
 });

38073d08e669abff254bd148e99b0dc3f66317af
Your Name <your-email@example.com>
2025-04-12 22:52:04 +0530

    bhiaaaaii

--- app.js
+++ app.js
@@ -118,7 +118,8 @@ app.use((err, req, res, next) => {
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
-    console.log(`Serving on port ${port}`)
-})
+//app.listen(port, () => {
+    //console.log(`Serving on port ${port}`)
+//})
 module.exports = app;
+ 
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// index.js
+/*index.js
 const app = require('./app');
 const mongoose = require('mongoose');
 
@@ -12,4 +12,13 @@ mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/railens', {
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
+});*/
+
+
+require('dotenv').config();
+const app = require('./app'); // importing the express app
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`✅ Server running on port ${port}`);
 });
